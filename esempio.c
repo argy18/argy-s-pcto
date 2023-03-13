da6bc4e37b36cf251238458a32c20b8220f7613a
argy18 <127745460+argy18@users.noreply.github.com>
2023-03-13 15:46:11 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+ int    numero = 18;
+ double numerolungo = 345321109;
+ float  numeroconlavirgola = 3.910;
+ char   carattere = 'a';
+
+ printf("il numero è: %d\n", numero);
+ printf("numero lungo è: %f, numero con la virgola è: %f\n", numerolungo, numeroconlavirgola);
+ printf("il carattere è %c\n", carattere);
+}
